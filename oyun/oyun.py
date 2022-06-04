b06d3aa8431c1e54080fb633944cb9914775bc95
barisirgin <irgin977@gmail.com>
2022-06-04 13:52:18 +0300

    Harf Hatirlama
    Belirli harf kümesinden rastgele ekrana basilan harften 2 önceki ile aynı ise dogru, degilse yanlis isaretledigimiz ve puan kazandigimiz bir oyun denemesi.

---
+++ oyun/oyun.py
@@ -0,0 +1,45 @@
+import random as rd
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+import time
+
+print(f"{Fore.RED}\t\t\t\t\tHoşgeldiniz")
+print(f"{Fore.LIGHTYELLOW_EX}Oyunun amacı rastgele ekrana gelen harften iki önceki harfle aynı olup olmadığının hatırlanmasıdır. Bol şans")
+print(f"{Fore.MAGENTA} - 0 = Doğru , 1 = Yanlış demektir.\n")
+harfler = "AEGIJMX" #
+liste_harf = []
+liste_cevap = [0,0]
+score = 0
+
+dogru = 0
+yanlis = 2
+k = 0
+
+for i in range(80):
+    a = rd.choice(harfler)
+    liste_harf.append(a)
+
+while True:
+    if k==78:
+        break
+    elif liste_harf[k] == liste_harf[k+2]:
+        dogru += 1
+        liste_cevap.append(1)
+    else:
+        yanlis += 1
+        liste_cevap.append(0)
+    k+=1
+
+for i in range(len(liste_harf)):
+    print(f"{Fore.BLUE}                    {i+1} / 80")
+    x = input(f"{liste_harf[i]} : ")
+    if x == str(liste_cevap[i]):
+        print(f"Doğru Cevap ")
+        score += 1
+    else:
+        print("Yanlis Cevap")
+    time.sleep(0.7)
+
+print(f"\nPuanınız : {score}")
+
